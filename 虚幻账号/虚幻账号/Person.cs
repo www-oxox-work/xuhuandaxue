1907e69b49b3976d2835e489cd6a2025e6eb6d8e
www-oxox-work <huangbangqing12@gmail.com>
2017-02-25 11:49:06 +0800

    Split name into first name and last name

--- 虚幻账号/虚幻账号/Person.cs
+++ 虚幻账号/虚幻账号/Person.cs
@@ -8,7 +8,8 @@ namespace 虚幻账号
 {
     class Person
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
 }
